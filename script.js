c5b738336cca9ca81cf69f68917b7ea3a5c443d9
Cori Beecroft <CoriBeecroft@users.noreply.github.com>
2020-04-04 12:58:27 -0700

    Wrote the block placement algorithm. Improved styling.

--- script.js
+++ script.js
@@ -1,77 +1,17 @@
 const container = document.getElementById("container");
 
 const FallingBlocks = () => {
-
+    const squares = [];
+    for(let i=0; i<64; i++) {
+        const color = (((Math.floor(i/8) % 2 + i) % 2) == 0) ? "color1" : "color2";
+        squares.push(<div className={ "grid-square " + color } />);
+    }
     return <div className="grid-container">
-        <div className="grid">
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-            <div className="grid-square" />
-        </div>
+        <div className="grid">{ squares }</div>
         <Blocks />
     </div>
 }
+
 class Blocks extends React.Component {
     constructor(props) {
         super(props);
@@ -98,13 +38,17 @@ class Blocks extends React.Component {
     }
 
     intersects(block1, block2) {
-        return block1.x <= block2.x && block2.x <= (block1.x + block1.size) ||
-            block2.x <= block1.x && block1.x <= (block2.x + block2.size)
+        if(block1.y != block2.y) {
+            return false;
+        } if ((block1.x <= block2.x) && (block2.x <= (block1.x + block1.size))) {
+            return true;
+        } else if ((block2.x <= block1.x) && (block1.x <= (block2.x + block2.size - 1))) {
+            return true;
+        } else { return false; }
     }
 
     generateBlockSizes() {
-        let spacesToBeFilled = 8 - Math.floor(Math.random() * 7) + 1
-        // console.log(spacesToBeFilled);
+        let spacesToBeFilled = 8 - (Math.floor(Math.random() * 7) + 1);
         let sizes = [];
 
         while(spacesToBeFilled > 0) {
@@ -117,67 +61,76 @@ class Blocks extends React.Component {
     }
 
     consolidateBlocks() {
-        this.clearRight();
+        return this.clearRight();
         // clearLeft();
         // clearCenter();
     }
 
     clearRight() {
-        let position = 0;
         this.blockPositions.sort((b1, b2) => {
-            console.log("in sort", );
-            if(b2.x == b1.x) {
-                return 0;
-            } if(b2.x > b1.x) {
-                return -1;
-            } else { return 1}
+            if(b2.x == b1.x) { return 0; }
+            if(b2.x > b1.x) { return -1; }
+            else { return 1; }
         })
 
-        console.log(JSON.stringify(this.blockPositions.map(p => p.x)));
-
-        for(let i=0; i<this.blockPositions; i++) {
-            let block = this.blockPositions[i];
-            block.x = position;
-            position = block.x + block.size;
+        let position = 0;
+        for(let i=0; i<this.blockPositions.length; i++) {
+            this.blockPositions[i].x = position;
+            position = this.blockPositions[i].x + this.blockPositions[i].size;
         }
+
+        return { min: position, max: 7 }
+    }
+
+    generateBlocks() {
+        let blocks = this.generateBlockSizes().map(size => {
+            let x = Math.floor(Math.random() * (8 - size));
+            let y = 7;
+            let count = 0; 
+            while(this.intersectsBlocks({ x: x, y: y, size: size }) && count < 200) {
+                if(count > 10) {
+                    let freeRange = this.consolidateBlocks();
+                    let freeSize = freeRange.max - freeRange.min;
+                    x = Math.floor(Math.random() * (freeSize - size) + freeRange.min);
+                } else {
+                    x = Math.floor(Math.random() * (8 - size));
+                }
+
+                count++;
+            }
+
+            if(count >= 200) { console.error("Error positioning blocks."); }
+
+            let position =  {
+                x: x,
+                y: y,
+                size: size
+            }
+            this.blockPositions.push(position);
+            return position;
+        });
+
+        return blocks;
     }
 
     render() {
         return <div className="blocks">
-            { this.generateBlockSizes().map((size, i) => <Block { ...{
-                size: size,
-                blockPositions: this.blockPositions,
-                intersectsBlocks: this.intersectsBlocks,
-                addBlockPosition: this.addBlockPosition,
-                y: i, 
-                consolidateBlocks: this.consolidateBlocks
+            { this.generateBlocks().map(block => <Block { ...{
+                size: block.size,
+                x: block.x,
+                y: block.y, 
             }} />) }
         </div>
     }
 }
 
 const Block = props => {
-    let x = Math.floor(Math.random() * (8 - props.size));
-    let y = props.y;//7// Math.floor(Math.random() * 8);
-
-    let count = 0; 
-    while(props.intersectsBlocks({ x: x, y: y, size: props.size }) && count < 100) {
-        if(count > 10) {
-            console.log("???");
-            props.consolidateBlocks();
-            // break;
-        }
-        count++;
-        x = Math.floor(Math.random() * (8 - props.size));
-    }
-
-    props.addBlockPosition({ x: x, y: y, size: props.size });
     return <div className="block" style={{
-        width: props.size*75,
+        width: props.size * 75,
         height: 75, 
-        top: y * 75 + 8,
-        left: x * 75 + 8, 
-        borderColor: "rgb(" + (Math.random()*128 + 128) + ", " + (Math.random()*128 + 128) + ", " + (Math.random()*128 + 128) + ")"
+        top: props.y * 75 + 8,
+        left: props.x * 75 + 8, 
+        backgroundColor: "rgb(" + (Math.random()*128 + 128) + ", " + (Math.random()*128 + 128) + ", " + (Math.random()*128 + 128) + ")"
     }} />
 }
 
